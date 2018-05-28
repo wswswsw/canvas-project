da27833c51edf7b84ed0cf462972e1995297fe46
wswswsw <wictorwilllumsen@gmail.com>
2018-05-28 11:53:31 +0800

    ellipse skeleton

--- assets/data/drawing-ellipse.js
+++ assets/data/drawing-ellipse.js
@@ -1,1 +1,31 @@
 //incomplete
+
+class DrawingEllipse extends PaintFunction{
+    constructor(contextReal,contextDraft){
+        super();
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;            
+    }
+    
+    onMouseDown(coord,event){
+        this.contextReal.strokeStyle = "#000";
+        this.origX = coord[0];
+        this.origY = coord[1];
+    }
+
+    onDragging(coord,event){
+        this.contextDraft.strokeStyle = "#000";
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+    }
+
+    onMouseMove(){}
+
+    onMouseUp(coord){
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextReal.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+    }
+    
+    onMouseLeave(){}
+    onMouseEnter(){}
+}
