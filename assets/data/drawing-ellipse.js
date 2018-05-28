86838909407f52bfdeed5e885777f0462a8fc4c5
wswswsw <wictorwilllumsen@gmail.com>
2018-05-28 13:03:16 +0800

    ellipse

--- assets/data/drawing-ellipse.js
+++ assets/data/drawing-ellipse.js
@@ -9,21 +9,25 @@ class DrawingEllipse extends PaintFunction{
     
     onMouseDown(coord,event){
         this.contextReal.strokeStyle = "#000";
+        this.contextReal.beginPath();
         this.origX = coord[0];
         this.origY = coord[1];
     }
 
     onDragging(coord,event){
         this.contextDraft.strokeStyle = "#000";
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height); //- to clear draft context before redrawing
+        this.contextDraft.beginPath();        
+        this.contextDraft.ellipse(this.origX, this.origY, Math.abs(coord[0]- this.origX), Math.abs(coord[1] - this.origY), 0 * Math.PI/180, 0, 2 * Math.PI) // rotation hardcoded as zero - essentially drawing an arc from zero @ x-axis to 2pi;
+        this.contextDraft.stroke();
     }
 
     onMouseMove(){}
 
     onMouseUp(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextReal.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextReal.ellipse(this.origX, this.origY, Math.abs(coord[0]- this.origX), Math.abs(coord[1] - this.origY), 0* Math.PI/180, 0, 2 * Math.PI)// rotation hardcoded as zero - use a JS text promt for user feedback?;
+        this.contextReal.stroke();
     }
     
     onMouseLeave(){}
