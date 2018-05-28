a5f4a48bd46054179cba42eae3c85499d4421c1b
wswswsw <wictorwilllumsen@gmail.com>
2018-05-28 13:20:16 +0800

    eraser

---
+++ assets/data/erase.js
@@ -0,0 +1,38 @@
+//incomplete
+
+class Erase extends PaintFunction {
+    constructor(contextReal) {
+        super();
+        this.context = contextReal;
+        this.contextDraft = contextDraft;
+    }
+
+    onMouseDown(coord, event) {
+        this.context.strokeStyle = "#000";
+        this.context.lineJoin = "round";
+        this.context.lineWidth = 5;
+        this.context.beginPath();
+        this.context.moveTo(coord[0], coord[1]);
+        this.draw(coord[0], coord[1]);
+    }
+
+    onDragging(coord, event) {
+        this.context.globalCompositeOperation = "destination-out";
+      
+        this.draw(coord[0], coord[1]);
+
+        this.context.globalCompositeOperation = "source-over";
+    }
+
+    onMouseMove() { }
+    onMouseUp() { }
+    onMouseLeave() { }
+    onMouseEnter() { }
+
+    draw(x, y) {
+        this.context.lineTo(x, y);
+        this.context.moveTo(x, y);
+        this.context.closePath();
+        this.context.stroke();
+    }
+}
