a341f00e39aa6e2127b1db0c69369e886967349e
wswswsw <wictorwilllumsen@gmail.com>
2018-05-30 15:38:04 +0800

    css change

--- assets/data/canvas-common.js
+++ assets/data/canvas-common.js
@@ -2,6 +2,8 @@ let canvasReal = document.getElementById('canvas-real');
 let contextReal = canvasReal.getContext('2d');
 let canvasDraft = document.getElementById('canvas-draft');
 let contextDraft = canvasDraft.getContext('2d');
+
+
 let currentFunction;
 let dragging = false;
 
--- assets/data/canvas-settings.js
+++ assets/data/canvas-settings.js
@@ -2,3 +2,4 @@ let strokeColor = '#000';
 $('#colorStroke').change((e) => {
     strokeColor = '#' + e.target.value;
 })
+
