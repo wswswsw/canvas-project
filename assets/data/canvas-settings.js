5cc7243ad6aea757a3f3fa678ace4387d5b2c5e9
wswswsw <wictorwilllumsen@gmail.com>
2018-05-30 16:57:07 +0800

    refresh

--- assets/data/canvas-settings.js
+++ assets/data/canvas-settings.js
@@ -3,12 +3,6 @@ $('#colorStroke').change((e) => {
     strokeColor = '#' + e.target.value;
 })
 
-
-// let bgColor = 
-// $("#canvas-real").css({
-//     "backgroundColor": "black"
-// });
-
 let bgColor = "ivory";
 $('#backgroundColor').change((e) => {
     $("#canvas-real").css({
