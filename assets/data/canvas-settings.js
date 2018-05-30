a73d3fce044da2cfc6373d7cf6de42d9dce932cf
wswswsw <wictorwilllumsen@gmail.com>
2018-05-30 16:18:48 +0800

    background colowerowier

--- assets/data/canvas-settings.js
+++ assets/data/canvas-settings.js
@@ -3,3 +3,15 @@ $('#colorStroke').change((e) => {
     strokeColor = '#' + e.target.value;
 })
 
+
+// let bgColor = 
+// $("#canvas-real").css({
+//     "backgroundColor": "black"
+// });
+
+let bgColor = "ivory";
+$('#backgroundColor').change((e) => {
+    $("#canvas-real").css({
+        "backgroundColor": backgroundColor = '#' + e.target.value
+    }); 
+})
