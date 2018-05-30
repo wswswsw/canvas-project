6e4e360582f3e766c96ed7386729db9999623561
Sonya Wong <32408750+sonyawongpro@users.noreply.github.com>
2018-05-30 16:29:29 +0800

    Update clear.js

--- assets/data/clear.js
+++ assets/data/clear.js
@@ -1,14 +1,10 @@
-      //Should I write a new function for this clear feature??//
-
-class Clear extends PaintFunction{
+class Clear extends PaintFunction {
     constructor(contextReal,contextDraft){
         super();
         this.contextReal = contextReal;
-        this.contextDraft = contextDraft;            
-    }
+        this.contextDraft = contextDraft; 
+        this.contextReal.clearRect(0,0,canvasReal.width,canvasReal.height);
+        this.contextReal.clearRect(0,0,canvasReal.width,canvasReal.height);
     
-//use the clearRect function 
-$("#resetCanvas").click(function(){
-        //Used the [0] to select the correct element in canvas
-    context.clearRect(0,0, canvas[0].width, canvas[0].height); 
-});
+    };
+}
