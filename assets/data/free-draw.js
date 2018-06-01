46ee9eef3e7ee22f8a618de69ba1055bde8a2be8
wswswsw <wictorwilllumsen@gmail.com>
2018-06-01 15:36:51 +0800

    1 bug fixed

--- assets/data/free-draw.js
+++ assets/data/free-draw.js
@@ -8,7 +8,6 @@ class FreeDraw extends PaintFunction {
     onMouseDown(coord, event) {
         this.context.strokeStyle = strokeColor;
         this.context.lineJoin = "round";
-        this.context.lineWidth = 1;
         this.context.beginPath();
         this.context.moveTo(coord[0], coord[1]);
         this.draw(coord[0], coord[1]);
