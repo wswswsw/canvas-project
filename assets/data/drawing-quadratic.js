235606908dd16ec736ff634a6b7f44e6f97c21a8
wswswsw <wictorwilllumsen@gmail.com>
2018-06-04 08:56:56 +0800

    quadratic curve errors reomoved

--- assets/data/drawing-quadratic.js
+++ assets/data/drawing-quadratic.js
@@ -48,53 +48,6 @@ class DrawingQuadratic extends PaintFunction {
 
       this.contextDraft.stroke();
     }
-
-    var layer = new Konva.Layer();
-    var dragLayer = new Konva.Layer();
-
-    for (var n = 0; n < 30; n++) {
-      addStar(layer, stage);
-    }
-
-    stage.add(layer, dragLayer);
-
-    stage.on("dragstart", function (evt) {
-      var shape = evt.target;
-      // moving to another layer will improve dragging performance
-      shape.moveTo(dragLayer);
-      stage.draw();
-
-      if (tween) {
-        tween.pause();
-      }
-
-      shape.setAttrs({
-        shadowOffset: {
-          x: 15,
-          y: 15
-        },
-
-        scale: {
-          x: shape.getAttr("startScale") * 1.2,
-          y: shape.getAttr("startScale") * 1.2
-        }
-
-      });
-    });
-
-    stage.on("dragend", function (evt) {
-      var shape = evt.target;
-      shape.moveTo(layer);
-      stage.draw();
-      shape.to({
-        duration: 0.5,
-        easing: Konva.Easings.ElasticEaseOut,
-        scaleX: shape.getAttr("startScale"),
-        scaleY: shape.getAttr("startScale"),
-        shadowOffsetX: 5,
-        shadowOffsetY: 5
-      });
-    });
   }
 
   onMouseMove() { }
@@ -114,7 +67,6 @@ class DrawingQuadratic extends PaintFunction {
 
       this.contextReal.stroke();
       this.actionCounter = 0;
-      this.onFinish();
     }
   }
 
